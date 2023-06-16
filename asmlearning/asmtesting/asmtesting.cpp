0b1fbfa01a2cb7d5e2eefc2b6d74e3f6aa20962d
msifanguy <musicmancorley@gmail.com>
2023-06-16 19:19:03 -0400

    stuff

--- asmlearning/asmtesting/asmtesting.cpp
+++ asmlearning/asmtesting/asmtesting.cpp
@@ -12,15 +12,28 @@ https://stackoverflow.com/questions/39968807/storing-dword-into-address
 int main()
 {
     LPCSTR mycmd = "cmd.exe";
-    std::cout << "Hello World!!!\n";
+    
     HMODULE kernel32addr = LoadLibrary(TEXT("kernel32.dll"));
-    printf("0x%p\n", kernel32addr);
+    printf("Kernel32 Address: 0x%p\n", kernel32addr);
+
     //HMODULE k2 = GetModuleHandle(L"ntdll.dll");
     //printf("0x%p\n", k2);
     FARPROC exitprocaddr=GetProcAddress(kernel32addr, "ExitProcess");
-    printf("0x%p\n", exitprocaddr);
+    printf("ExitProcess Address: 0x%p\n", exitprocaddr);
     FARPROC winexecaddr = GetProcAddress(kernel32addr, "WinExec");
-    printf("0x%p\n", winexecaddr);
+    printf("WinExec Address: 0x%p\n", winexecaddr);
+
+    FARPROC getprocaddr = GetProcAddress(kernel32addr, "GetProcAddress");
+    printf("GetProcAddress Address: 0x%p\n", getprocaddr);
+
+    FARPROC loadlibaddr = GetProcAddress(kernel32addr, "LoadLibraryA");
+    printf("LoadLibrary Address: 0x%p\n", loadlibaddr);
+
+    HMODULE user32addr = LoadLibrary(TEXT("user32.dll"));
+    printf("User32 Address: 0x%p\n", user32addr);
+
+    FARPROC msgboxaddr = GetProcAddress(user32addr, "MessageBoxA");
+    printf("MessageBox Address: 0x%p\n", msgboxaddr);
 
     _asm
     {
